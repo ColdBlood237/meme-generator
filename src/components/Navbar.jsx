2b6686279d05bd39b7d443405bd9eddd30383aea
Pixel <petngalan@gmail.com>
2023-12-30 22:57:00 +0100

    Initialize project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import trollFace from "../assets/troll-face.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src=[trollFace] alt="" />
+      <span>Meme Generator</span>
+    </nav>
+  );
+}
