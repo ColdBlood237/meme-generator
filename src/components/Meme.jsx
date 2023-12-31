28d5a7fc5143670ec1075c0887b0ba668f3991b0
Pixel <petngalan@gmail.com>
2023-12-31 13:13:24 +0100

    Center the image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -23,7 +23,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} />
+      <img src={memeImage} className="meme-image" />
     </main>
   );
 }
